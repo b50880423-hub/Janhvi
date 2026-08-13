f951e05e523922c355b8b37734e66b5aa067d6fa
b50880423-hub <b50880423@gmail.com>
2026-08-14 01:44:49 +0530

    Delete utils directory

--- utils/permissions.py
+++
@@ -1,8 +0,0 @@
-from telegram.constants import ChatMemberStatus
-
-async def is_admin(bot, chat_id, user_id):
-    try:
-        member = await bot.get_chat_member(chat_id, user_id)
-        return member.status in (ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.OWNER)
-    except Exception:
-        return False
